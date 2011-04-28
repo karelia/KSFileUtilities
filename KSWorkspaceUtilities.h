a3b7edffa3d39a5397f87ac6d1446ae26ab8a093
Mike Abdullah <me@mikeabdullah.net>
2011-04-28 16:41:56 +0100

    KSWorkspaceUtilities

---
+++ KSWorkspaceUtilities.h
@@ -0,0 +1,14 @@
+//
+//  KSWorkspaceUtilities.h
+//  Sandvox
+//
+//  Created by Mike on 28/04/2011.
+//  Copyright 2005-2011 Karelia Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSWorkspace (KSWorkspaceUtilities)
+
+@end
---
+++ KSWorkspaceUtilities.m
@@ -0,0 +1,14 @@
+//
+//  KSWorkspaceUtilities.m
+//  Sandvox
+//
+//  Created by Mike on 28/04/2011.
+//  Copyright 2011 Karelia Software. All rights reserved.
+//
+
+#import "KSWorkspaceUtilities.h"
+
+
+@implementation NSWorkspace (KSWorkspaceUtilities)
+
+@end
