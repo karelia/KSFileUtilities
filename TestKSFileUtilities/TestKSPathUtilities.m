08bfa1d3b89a046fbd7627275bf156da9b20b99d
Abizer Nasir <abizern@abizern.org>
2011-05-22 18:45:56 +0100

    Remove boilerplate from the test class

--- TestKSFileUtilities/TestKSPathUtilities.m
+++ TestKSFileUtilities/TestKSPathUtilities.m
@@ -18,20 +18,5 @@
 
 @implementation TestKSPathUtilities
 
-- (void)setUp {
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown {
-    // Tear-down code here.
-    
-    [super tearDown];
-}
-
-- (void)testExample {
-    STFail(@"Unit tests are not implemented yet in TestKSPathUtilities");
-}
 
 @end
