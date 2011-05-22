b4f6d2b9b6486d979b9d80a2330f53f5a3266244
Abizer Nasir <abizern@abizern.org>
2011-05-22 19:13:17 +0100

    Create two simple test cases to for ks_pathRelativeToDirectory in KSPathUtilities

--- TestKSFileUtilities/TestKSPathUtilities.m
+++ TestKSFileUtilities/TestKSPathUtilities.m
@@ -6,6 +6,7 @@
 //
 
 #import <SenTestingKit/SenTestingKit.h>
+#import "KSPathUtilities.h"
 
 
 @interface TestKSPathUtilities : SenTestCase {
@@ -18,5 +19,29 @@
 
 @implementation TestKSPathUtilities
 
+#pragma mark - Common test strings
+
+NSString * const pathRoot = @"/";
+NSString * const pathFoo = @"/foo";
+NSString * const pathFooBar = @"/foo/bar";
+
+
+
+#pragma mark - Tests
+
+- (void)testPathRelativeToDirectory {
+    // Test cases for ks_pathRelativeToDirectory
+    NSString *result;
+    NSString *expectedResult;
+
+    result = [pathFoo ks_pathRelativeToDirectory:pathRoot];
+    expectedResult = @"foo";
+    STAssertTrue([result isEqualToString:expectedResult], @"\'%@\' relative to \'%@\' should be \'%@\' instead of \'%@\'", pathFoo, pathRoot, expectedResult, result);
+    
+    
+    result = [pathFoo ks_pathRelativeToDirectory:pathFooBar];
+    expectedResult = @"..";
+    STAssertTrue([result isEqualToString:expectedResult], @"\'%@\' relative to \'%@\' should be \'%@\' instead of \'%@\'", pathFoo, pathRoot, expectedResult, result);
+}
 
 @end
