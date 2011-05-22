66a501d0a7b7f4e6b035ad1d4b179ac465f28fed
Abizer Nasir <abizern@abizern.org>
2011-05-22 18:38:13 +0100

    Create a basic project for unit testing
    At the moment - this only exercises KSPathUtilities.

---
+++ TestKSFileUtilities/TestKSPathUtilities.m
@@ -0,0 +1,37 @@
+//
+//  TestKSPathUtilities.m
+//  KSFileUtilities
+//
+//  Created by Abizer Nasir on 22/05/2011.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface TestKSPathUtilities : SenTestCase {
+@private
+    
+}
+
+@end
+
+
+@implementation TestKSPathUtilities
+
+- (void)setUp {
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown {
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample {
+    STFail(@"Unit tests are not implemented yet in TestKSPathUtilities");
+}
+
+@end
