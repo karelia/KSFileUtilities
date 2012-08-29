64fead37fe0a174480ffbab830374f88fa254bb1
Mike Abdullah <me@mikeabdullah.net>
2012-08-29 10:57:25 +0100

    Bring in email tests from http://pgregg.com/projects/php/code/showvalidemail.php

--- TestKSFileUtilities/TestKSURLFormatter.m
+++ TestKSFileUtilities/TestKSURLFormatter.m
@@ -55,6 +55,53 @@
     [self testAllowedSchemesWithString:@"test test/" expectedURLString:@"http://test%20test.com/"];
 }
 
+- (void)testValidEmailAddress
+{
+    // Test from http://pgregg.com/projects/php/code/showvalidemail.php
+    // I've commented out those that fail at present; we're not all that bothered about getting this spot on!
+    
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"name.lastname@domain.com"], nil);
+    STAssertFalse([KSURLFormatter isValidEmailAddress:@".@"], nil);
+    STAssertFalse([KSURLFormatter isValidEmailAddress:@"a@b"], nil);
+    STAssertFalse([KSURLFormatter isValidEmailAddress:@"@bar.com"], nil);
+    STAssertFalse([KSURLFormatter isValidEmailAddress:@"@@bar.com"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"a@bar.com"], nil);
+    STAssertFalse([KSURLFormatter isValidEmailAddress:@"aaa.com"], nil);
+    STAssertFalse([KSURLFormatter isValidEmailAddress:@"aaa@.com"], nil);
+    STAssertFalse([KSURLFormatter isValidEmailAddress:@"aaa@.123"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"aaa@[123.123.123.123]"], nil);
+    //STAssertFalse([KSURLFormatter isValidEmailAddress:@"aaa@[123.123.123.123]a"], nil);   // extra data outside ip
+    //STAssertFalse([KSURLFormatter isValidEmailAddress:@"aaa@[123.123.123.333]"], nil);    // not a valid IP
+    //STAssertFalse([KSURLFormatter isValidEmailAddress:@"a@bar.com."], nil);
+    STAssertFalse([KSURLFormatter isValidEmailAddress:@"a@bar"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"a-b@bar.com"], nil);
+    STAssertFalse([KSURLFormatter isValidEmailAddress:@"+@b.c"], nil);    // min 2 char tld
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"+@b.com"], nil);
+    //STAssertFalse([KSURLFormatter isValidEmailAddress:@"a@-b.com"], nil);
+    //STAssertFalse([KSURLFormatter isValidEmailAddress:@"a@b-.com"], nil);
+    STAssertFalse([KSURLFormatter isValidEmailAddress:@"-@..com"], nil);
+    //STAssertFalse([KSURLFormatter isValidEmailAddress:@"-@a..com"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"a@b.co-foo.uk"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"\"hello my name is\"@stutter.com"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"\"Test \\\"Fail\\\" Ing\"@example.com"], nil); // not sure I understood this one
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"valid@special.museum"], nil);
+    //STAssertFalse([KSURLFormatter isValidEmailAddress:@"invalid@special.museum-"], nil);
+    //STAssertFalse([KSURLFormatter isValidEmailAddress:@"shaitan@my-domain.thisisminekthx"], nil); // tld way too long
+    STAssertFalse([KSURLFormatter isValidEmailAddress:@"test@...........com"], nil);
+    //STAssertFalse([KSURLFormatter isValidEmailAddress:@"foobar@192.168.0.1"], nil); // ip need to be [] from reading http://haacked.com/archive/2007/08/21/i-knew-how-to-validate-an-email-address-until-i.aspx
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"\"Abc\\@def\"@example.com"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"\"Fred Bloggs\"@example.com"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"\"Joe\\Blow\"@example.com"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"\"Abc@def\"@example.com"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"customer/department=shipping@example.com"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"$A12345@example.com"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"!def!xyz%abc@example.com"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"_somename@example.com"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"Test \\ Folding \\ Whitespace@example.com"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"HM2Kinsists@(that comments are allowed)this.is.ok"], nil);
+    STAssertTrue([KSURLFormatter isValidEmailAddress:@"user%uucp!path@somehost.edu"], nil);
+}
+
 - (void)testLikelyEmailAddress
 {
     STAssertFalse([KSURLFormatter isLikelyEmailAddress:@"http://example.com@foo.com"], @"It's a *valid* email address, but more likely to be a URL");
