bb25b102ac7c987e07361ea32f3d5fec37d2c90c
Mike Abdullah <me@mikeabdullah.net>
2012-11-11 10:53:51 +0000

    Few other, stricter IDN tests.

--- TestKSFileUtilities/TestKSURLFormatter.m
+++ TestKSFileUtilities/TestKSURLFormatter.m
@@ -58,6 +58,8 @@
 - (void)testInternationalizedDomainName
 {
     [self testAllowedSchemesWithString:@"http://exämple.com" expectedURLString:@"http://xn--exmple-cua.com/"];
+    [self testAllowedSchemesWithString:@"exämple.com" expectedURLString:@"http://xn--exmple-cua.com/"];
+    [self testAllowedSchemesWithString:@"exämple" expectedURLString:@"http://xn--exmple-cua.com/"];
 }
 
 - (void)testDoubleFragment;
