9eb3a74483d79a7b3a66399f9b40fa2639a906e1
Mike Abdullah <me@mikeabdullah.net>
2013-07-06 12:51:16 +0100

    First batch of tests

---
+++ TestKSFileUtilities/TestKSURLComponents.m
@@ -0,0 +1,68 @@
+//
+//  TestKSURLComponents.m
+//  KSFileUtilities
+//
+//  Created by Mike on 06/07/2013.
+//  Copyright (c) 2013 Jungle Candy Software. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+#import "KSURLComponents.h"
+
+@interface TestKSURLComponents : SenTestCase
+
+@end
+
+@implementation TestKSURLComponents
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testInit
+{
+    KSURLComponents *components = [[KSURLComponents alloc] init];
+    
+    STAssertNil(components.scheme, nil);
+    STAssertNil(components.user, nil);
+    STAssertNil(components.percentEncodedUser, nil);
+    STAssertNil(components.password, nil);
+    STAssertNil(components.percentEncodedPassword, nil);
+    STAssertNil(components.host, nil);
+    STAssertNil(components.percentEncodedHost, nil);
+    STAssertNil(components.port, nil);
+    STAssertNil(components.path, nil);
+    STAssertNil(components.percentEncodedPath, nil);
+    STAssertNil(components.query, nil);
+    STAssertNil(components.percentEncodedQuery, nil);
+    STAssertNil(components.fragment, nil);
+    STAssertNil(components.percentEncodedFragment, nil);
+    
+    [components release];
+}
+
+- (void)testInitWithAbsoluteURL;
+{
+    KSURLComponents *components = [KSURLComponents componentsWithURL:[NSURL URLWithString:@"scheme://user:password@host:0/path?query#fragment"]
+                                             resolvingAgainstBaseURL:NO];
+    
+    STAssertEqualObjects(components.scheme, @"scheme", nil);
+    STAssertEqualObjects(components.user, @"user", nil);
+    STAssertEqualObjects(components.password, @"password", nil);
+    STAssertEqualObjects(components.host, @"host", nil);
+    STAssertEqualObjects(components.port, @(0), nil);
+    STAssertEqualObjects(components.path, @"path", nil);
+    STAssertEqualObjects(components.query, @"query", nil);
+    STAssertEqualObjects(components.fragment, @"fragment", nil);
+}
+
+@end
