ad69038e7b02c1a10a765dbd3f3c350b68d00b50
Mike Abdullah <me@mikeabdullah.net>
2013-12-12 10:12:39 +0000

    Internally hand off KSURLComponents' query parameter handling to KSURLQuery

--- KSURLComponents.m
+++ KSURLComponents.m
@@ -26,6 +26,8 @@
 
 #import "KSURLComponents.h"
 
+#import "KSURLQuery.h"
+
 
 @interface KSURLComponents ()
 @property (nonatomic, copy, readwrite) NSString *percentEncodedUser;
@@ -570,121 +572,20 @@
 
 - (NSDictionary *)queryParametersWithOptions:(KSURLComponentsQueryParameterDecodingOptions)options;
 {
-    __block NSMutableDictionary *result = [NSMutableDictionary dictionary];
-    
-    [self enumerateQueryParametersWithOptions:options usingBlock:^(NSString *key, NSString *value, BOOL *stop) {
-        
-        // Bail if doesn't fit dictionary paradigm
-        if (!value || [result objectForKey:key])
-        {
-            *stop = YES;
-            result = nil;
-            return;
-        }
-        
-        [result setObject:value forKey:key];
-    }];
-    
-    return result;
+    return [[KSURLQuery queryWithPercentEncodedString:self.percentEncodedQuery] parametersWithOptions:options];
 }
 
 - (void)setQueryParameters:(NSDictionary *)parameters;
 {
-    if (!parameters)
-    {
-        self.query = nil;
-        return;
-    }
-    
-    // Build the list of parameters as a string
-	NSMutableString *query = [NSMutableString string];
-	
-    NSEnumerator *enumerator = [parameters keyEnumerator];
-    BOOL thisIsTheFirstParameter = YES;
-    
-    NSString *key;
-    while ((key = [enumerator nextObject]))
-    {
-        CFStringRef escapedKey = CFURLCreateStringByAddingPercentEscapes(NULL, (CFStringRef)key, NULL, CFSTR("+=&#"), kCFStringEncodingUTF8);
-        // Escape + for safety as some backends interpret it as a space
-        // = indicates the start of value, so must be escaped
-        // & indicates the start of next parameter, so must be escaped
-        // # indicates the start of fragment, so must be escaped
-        
-        NSString *parameter = [parameters objectForKey:key];
-        
-        // Append the parameter and its key to the full query string
-        if (!thisIsTheFirstParameter)
-        {
-            [query appendString:@"&"];
-        }
-        else
-        {
-            thisIsTheFirstParameter = NO;
-        }
-        
-        CFStringRef escapedValue = CFURLCreateStringByAddingPercentEscapes(NULL, (CFStringRef)parameter, NULL, CFSTR("+&#"), kCFStringEncodingUTF8);
-        // Escape + for safety as some backends interpret it as a space
-        // = is allowed in values, as there's no further value to indicate
-        // & indicates the start of next parameter, so must be escaped
-        // # indicates the start of fragment, so must be escaped
-        
-        [query appendFormat:@"%@=%@", escapedKey, escapedValue];
-        
-        CFRelease(escapedKey);
-        CFRelease(escapedValue);
-    }
-    
-    self.percentEncodedQuery = query;
+    KSURLQuery *query = [[KSURLQuery alloc] init];
+    [query setParameters:parameters];
+    self.percentEncodedQuery = query.percentEncodedString;
+    [query release];
 }
 
 - (void)enumerateQueryParametersWithOptions:(KSURLComponentsQueryParameterDecodingOptions)options usingBlock:(void (^)(NSString *key, NSString *value, BOOL *stop))block;
 {
-    BOOL stop = NO;
-    
-    NSString *query = self.percentEncodedQuery; // we'll do our own decoding after separating components
-    NSRange searchRange = NSMakeRange(0, query.length);
-    
-    while (!stop)
-    {
-        NSRange keySeparatorRange = [query rangeOfString:@"=" options:NSLiteralSearch range:searchRange];
-        if (keySeparatorRange.location == NSNotFound) keySeparatorRange = NSMakeRange(NSMaxRange(searchRange), 0);
-        
-        NSRange keyRange = NSMakeRange(searchRange.location, keySeparatorRange.location - searchRange.location);
-        NSString *key = [query substringWithRange:keyRange];
-        
-        NSString *value = nil;
-        if (keySeparatorRange.length)   // there might be no value, so report as nil
-        {
-            searchRange = NSMakeRange(NSMaxRange(keySeparatorRange), query.length - NSMaxRange(keySeparatorRange));
-            
-            NSRange valueSeparatorRange = [query rangeOfString:@"&" options:NSLiteralSearch range:searchRange];
-            if (valueSeparatorRange.location == NSNotFound)
-            {
-                valueSeparatorRange.location = NSMaxRange(searchRange);
-                stop = YES;
-            }
-            
-            NSRange valueRange = NSMakeRange(searchRange.location, valueSeparatorRange.location - searchRange.location);
-            value = [query substringWithRange:valueRange];
-            
-            searchRange = NSMakeRange(NSMaxRange(valueSeparatorRange), query.length - NSMaxRange(valueSeparatorRange));
-        }
-        else
-        {
-            stop = YES;
-        }
-        
-        if (options & KSURLComponentsQueryParameterDecodingPlusAsSpace)
-        {
-            key = [key stringByReplacingOccurrencesOfString:@"+" withString:@" "];
-            value = [value stringByReplacingOccurrencesOfString:@"+" withString:@" "];
-        }
-        
-        block([key stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding],
-              [value stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding],
-              &stop);
-    }
+    [[KSURLQuery queryWithPercentEncodedString:self.percentEncodedQuery] enumerateParametersWithOptions:options usingBlock:block];
 }
 
 @end
